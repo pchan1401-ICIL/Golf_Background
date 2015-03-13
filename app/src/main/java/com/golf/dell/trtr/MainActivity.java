7011b2b25585c85943bba6db5875902ccb2fa122
vivek <VIVEKSONIHMR@GMAIL.COM>
2015-03-13 13:00:10 +0900

    Edit

--- app/src/main/java/com/golf/dell/trtr/MainActivity.java
+++ app/src/main/java/com/golf/dell/trtr/MainActivity.java
@@ -28,13 +28,13 @@ public class MainActivity extends Activity implements Drawer {
 
         setContentView(surface);
 
-        int[] resourceIds = new int[] {R.drawable.golf,R.string.audiowide_font,R.drawable.vv,R.drawable.aa};
+        int[] resourceIds = new int[] {R.drawable.golf,R.string.audiowide_font};
 
 SpriteBatcher sb = new SpriteBatcher(this,resourceIds,this);
-        sb.setFontParams(R.string.audiowide_font,new FontParams().size(30));
+       sb.setFontParams(R.string.audiowide_font,new FontParams().size(15));
 
         surface.setRenderer(sb);
-
+//surface.setRenderer(new SpriteBatcher(this,resourceIds,this));
 
 
 
@@ -45,12 +45,12 @@ SpriteBatcher sb = new SpriteBatcher(this,resourceIds,this);
     public void onDrawFrame(GL10 gl, SpriteBatcher sb) {
         sb.draw(R.drawable.golf, new Rect(0,0,960,560), new Rect(0,0,sb.getViewWidth(),sb.getViewHeight()));
         sb.batchDraw(gl);
-        sb.draw(R.drawable.world, new Rect(0,0,101,101), new Rect(100,100,300,300));
+
 
         sb.draw(R.drawable.vv, new Rect(0,0,55,41),  new Rect(30,560,75,591));
-        sb.draw(R.drawable.aa, new Rect(0,0,713,392),  new Rect(2500,100,713,392));
+        //sb.draw(R.drawable.aa, new Rect(0,0,713,392),  new Rect(250,100,713,392));
 
-        sb.drawText(R.string.audiowide_font,"Hello",100,50,1f,0xffff0000);
+        sb.drawText(R.string.audiowide_font, "Hit_Ball",100,500,1f,0xffff0000);
 
     }
 }
